d443f8b8bb64d7db1b164a94bd0517a17682d555
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-08 15:20:21 +0000

    js added image

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+document.getElementById('mybtn').addEventListener('click', time)
+// Event Listener connects the button with the function "time()"
+
+function time () {
+  let Large = 6
+  let XL = 10
+  let One = 1
+  let Two = 1.75
+  let Three = 2.5
+  let Four = 3.25
+  let Tax = 1.13
+  let Subtotal = Size + Toppings
+    alert(Subtotal)
+    alert('total')
+    alert('totaltax')
+}
