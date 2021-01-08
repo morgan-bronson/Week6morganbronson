3d02954e82dfdfbf8d1890c883dffa6416e51ec6
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-08 22:11:45 +0000

    working with $

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ document.getElementById('mybtn').addEventListener('click', time)
 // Event Listener connects the button with the function "time()"
 
 function time () {
+  // Create our number formatter.
+  // Code from https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+  var formatter = new Intl.NumberFormat('en-CA', {
+    style: 'currency',
+    currency: 'CAD',
+  });
   let size = parseFloat(document.getElementById('size').value)
   let toppings = parseFloat(document.getElementById('toppings').value)
   let subtotal = size + toppings
   let tax = subtotal * .13
   let total = subtotal + tax
-  document.getElementById('subtotal').value = subtotal
-  document.getElementById('tax').value = tax
-  document.getElementById('total').value = total
+  console.log(formatter.format(total))
+  document.getElementById('subtotal').value = formatter.format(subtotal);   
+  document.getElementById('tax').value = tax = formatter.format(tax);
+  document.getElementById('total').value = total = formatter.format(total);
 }
