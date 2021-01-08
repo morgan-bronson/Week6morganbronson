7af91d54f42692ca06e7fe0552ce7bd079c6b421
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-08 23:14:42 +0000

    fixed errors

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ document.getElementById('mybtn').addEventListener('click', time)
 function time () {
   // Create our number formatter.
   // Code from https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
-  var formatter = new Intl.NumberFormat('en-CA', {
+  const formatter = new Intl.NumberFormat('en-CA', {
     style: 'currency',
-    currency: 'CAD',
-  });
-  let size = parseFloat(document.getElementById('size').value)
-  let toppings = parseFloat(document.getElementById('toppings').value)
-  let subtotal = size + toppings
-  let tax = subtotal * .13
+    currency: 'CAD'
+  })
+  const size = parseFloat(document.getElementById('size').value)
+  const toppings = parseFloat(document.getElementById('toppings').value)
+  const subtotal = size + toppings
+  let tax = subtotal * 0.13
   let total = subtotal + tax
   console.log(formatter.format(total))
-  document.getElementById('subtotal').value = formatter.format(subtotal);   
-  document.getElementById('tax').value = tax = formatter.format(tax);
-  document.getElementById('total').value = total = formatter.format(total);
+  document.getElementById('subtotal').value = formatter.format(subtotal)
+  document.getElementById('tax').value = tax = formatter.format(tax)
+  document.getElementById('total').value = total = formatter.format(total)
 }
