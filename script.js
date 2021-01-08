d86e1ef748cdf14e92174ff555a777eaf89ee435
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-08 22:03:07 +0000

    working

--- script.js
+++ script.js
@@ -4,17 +4,12 @@ document.getElementById('mybtn').addEventListener('click', time)
 // Event Listener connects the button with the function "time()"
 
 function time () {
-  let Large = 6
-  let XL = 10
-  let One = 1
-  let Two = 1.75
-  let Three = 2.5
-  let Four = 3.25
-  let Tax = 1.13
-  let Subtotal = Size + Toppings
-    alert(Subtotal)
-  let Total = Subtotal * Tax
-    alert('total')
-  let Totaltax = 
-    alert('totaltax')
+  let size = parseFloat(document.getElementById('size').value)
+  let toppings = parseFloat(document.getElementById('toppings').value)
+  let subtotal = size + toppings
+  let tax = subtotal * .13
+  let total = subtotal + tax
+  document.getElementById('subtotal').value = subtotal
+  document.getElementById('tax').value = tax
+  document.getElementById('total').value = total
 }
